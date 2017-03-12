45694c372c95f91def2fc656b6e170adc1e11353
Selwyn McCracken <selwyn.mccracken@gmail.com>
2017-03-13 10:57:03 +1300

    Fix getTable and make more robust (e.g. catching and handling zero record results).
    Add tests to atone for my sins ;-)

---
+++ tests/testthat/test-getGeomReturnsSF.R
@@ -0,0 +1,17 @@
+test_that(
+  'SF results returned when geometry requested',
+  {
+    expect_true(
+      'sf' %in% class(
+        getTableData(
+          getAuthorisedHeaders(userName='api_demo', password='thursday', database='RAMM API Demo'),
+          'carr_way',
+          get_geometry = TRUE,
+          filters = list(
+            createFilter('carr_way_no','LessThan',20)
+          )
+        )
+      )
+    )
+  }
+)
