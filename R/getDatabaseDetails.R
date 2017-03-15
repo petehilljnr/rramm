8aa3d283dea4184b1cc85cead4b519af3e473aee
Selwyn McCracken <selwyn.mccracken@gmail.com>
2017-03-15 20:21:23 +1300

    add httr prefix - lesson never import libraries in your Rprofile...

--- R/getDatabaseDetails.R
+++ R/getDatabaseDetails.R
@@ -1,24 +1,28 @@
 #' getDatabaseDetails Function
-#' 
+#'
 #' This returns the details of the database you are connected to via the headers (which contains the token)
 #' The returned object contains information regarding the map projection you may need to convert to/from
-#' 
+#'
 #' @param headers A headers object created using the getHeaders function
 #' @export
-#' @examples 
+#' @examples
 #' dbDetails <- getDatabaseDetails(headers=getHeaders(...))
 
 
 getDatabaseDetails = function(headers) {
   #  Function to return useful database information, especially the EPSG code ($mapProjectDetail)
   #  that should be used when doing geometry attribute functions
-  
+
   db_details_req  = httr::GET(
     'https://apps.ramm.co.nz:443/RammApi6.1/v1/database',
     headers
   )
-  
-  db_details = jsonlite::fromJSON(content(db_details_req,'text'))
-  
+
+  db_details = jsonlite::fromJSON(
+    httr::content(
+      db_details_req,
+      'text')
+  )
+
   return(db_details)
 }
